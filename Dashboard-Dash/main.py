c9672ac21993d069c50f3832177737d71ccf8a8c
Tom <tombk@web.de>
2022-04-03 12:15:38 +0300

    update

--- Dashboard-Dash/main.py
+++ Dashboard-Dash/main.py
@@ -1,1 +1,15 @@
-import pandas as pd
+import pandas as pd 
+import plotly.express as px
+import plotly.graph_objs as go
+
+import dash 
+from dash import dcc, html 
+from dash.dependencies import Input, Output
+
+app = dash.Dash(__name__)
+
+#-----------------------------------------------------
+# Import and clean data (importing csv into pandas)
+
+df = pd.read_csv('bees.csv')
+print(df)
